f6bced31657b7adc93747c004374791725c7f3aa
AlexisT37 <alexistroitzky@gmail.com>
2022-05-17 09:20:10 +0200

    queen

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php
-//this is a program that allows us to filter through an array of strings according to a letter
-declare(strict_types=1);
-class StartWith
-{
-    public function __construct(private string $startWith)
-    {
-        //construct allows us to quickly build the instance without having to declare the variables
-    }
 
-    public function __invoke(string $chaine)
+
+class TopChanteur
+
+{
+
+    public function __invoke(...$args)
+
     {
-        return str_starts_with($chaine, $this->startWith);
-        //invoke allows us to use the object as a function
-        //returns the result of the method where it starts with the chain
+
+        return ucwords(sprintf('%s %s%s', ...$args));
     }
 }
 
-var_dump(array_filter(["Scourge", "Firebrand", "Virtuoso", "Mechanist", "Mesmer", "Mage", "Meridian"], new StartWith("M")));
+
+class CharlyEtLulu
+{
+
+    public function __construct(private TopChanteur $topChanteur)
+    {
+    }
+
+
+    public function __call($method, $arguments)
+    {
+
+        return ($this->topChanteur)($method, ...$arguments);
+    }
+}
+
+$queen = (new CharlyEtLulu(new TopChanteur))->freddy('mer', 'cury');
+// $queen = (new CharlyEtLulu(new TopChanteur('Freddy'))->__call('Mer', 'ury');
+// $queen = (new CharlyEtLulu)->__call(new TopChanteur('Freddy'), 'Mer', 'cury');
+// $queen = new CharlyEtLulu(new TopChanteur)->freddy('mer', 'cury');
+echo $queen;
