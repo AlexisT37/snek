92c262fe362fd3e1695a4df8698183202c9421f6
AlexisT37 <alexistroitzky@gmail.com>
2022-05-17 09:06:56 +0200

    invoke

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-
+//this is a program that allows us to filter through an array of strings according to a letter
 declare(strict_types=1);
-class Majuscule
+class StartWith
 {
+    public function __construct(private string $startWith)
+    {
+        //construct allows us to quickly build the instance without having to declare the variables
+    }
+
     public function __invoke(string $chaine)
     {
-        return strToUpper($chaine);
+        return str_starts_with($chaine, $this->startWith);
+        //invoke allows us to use the object as a function
+        //returns the result of the method where it starts with the chain
     }
 }
-$enMajuscule = new Majuscule;
-echo $enMajuscule("chaine en minuscule");
+
+var_dump(array_filter(["Scourge", "Firebrand", "Virtuoso", "Mechanist", "Mesmer", "Mage", "Meridian"], new StartWith("M")));
